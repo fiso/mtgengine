fc8ecaba0a3cd5652f88b5ead7c3b4d2c697918f
Filip Haglund <filip.l.haglund@gmail.com>
2016-04-05 10:53:29 +0200

    Lightning Bolt implementation

--- engine/cards/sets/setATH/LightningBolt.js
+++ engine/cards/sets/setATH/LightningBolt.js
@@ -1,10 +1,16 @@
 "use strict";
 const Constants = require ("../../../Constants");
 const UnimplementedCard = require("../../UnimplementedCard");
+const assert = require("assert");
 
-class LightningBolt extends UnimplementedCard {
+class LightningBolt extends Card {
   constructor(game) {
     super(game, "Lightning Bolt", "Anthologies", "ATH");
+  }
+
+  resolve (controller, targets) {
+    assert(targets.length === 1);
+    targets[0].damage(3, this._guid, false);
   }
 }
 
