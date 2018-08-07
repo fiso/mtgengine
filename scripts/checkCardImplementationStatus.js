10a901007679d7ff34c533fdd9a3dcbe6d69a4a7
Filip Haglund <filip.l.haglund@gmail.com>
2018-08-07 14:18:50 +0200

    Add a depressing script

---
+++ scripts/checkCardImplementationStatus.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+const glob = require('glob');
+const fs = require('fs');
+
+glob('src/engine/cards/sets/**/*.js', function(err, files) {
+  console.log(`${files.length} cards total`);
+  let unimplemented = 0;
+  let implemented = 0;
+  let reprints = 0;
+  for (const file of files) {
+    const contents = fs.readFileSync(file);
+    if (contents.indexOf('extends UnimplementedCard') !== -1) {
+      unimplemented++;
+    } else if (contents.indexOf('extends Card') !== -1) {
+      implemented++;
+    } else {
+      reprints++;
+    }
+  }
+
+  const totalToImplement = implemented + unimplemented;
+
+  console.log(`${reprints} reprints`);
+  console.log(`${implemented} implemented cards`);
+  console.log(`${unimplemented} unimplemented cards`);
+  console.log(`${(implemented / totalToImplement * 100).toFixed(2)}% done`);
+});
